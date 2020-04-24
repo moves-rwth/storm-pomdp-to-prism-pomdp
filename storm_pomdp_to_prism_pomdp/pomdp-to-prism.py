5b0faf6aadddf72e8005904172358d4db24afe0c
Sebastian Junges <sebastian.junges@gmail.com>
2020-04-24 15:07:52 -0700

    refactoring and logging output

--- storm_pomdp_to_prism_pomdp/pomdp-to-prism.py
+++ storm_pomdp_to_prism_pomdp/pomdp-to-prism.py
@@ -3,6 +3,10 @@ import argparse
 import stormpy as sp
 import stormpy.pomdp
 
+import logging
+logger = logging.getLogger(__name__)
+logging.basicConfig(format='%(message)s', level=logging.INFO)
+
 
 def build_pomdp(program):
     options = sp.BuilderOptions([])
@@ -10,7 +14,9 @@ def build_pomdp(program):
     options.set_build_choice_labels()
     options.set_build_all_labels()
 
-    return sp.build_sparse_model_with_options(program, options)
+    model =  sp.build_sparse_model_with_options(program, options)
+    model = sp.pomdp.make_canonic(model)
+    return model
 
 def main():
     parser = argparse.ArgumentParser(description='Converter')
@@ -19,13 +25,14 @@ def main():
     parser.add_argument('--output', '-o', help='Model file', required=True)
     args = parser.parse_args()
 
+    logger.info("Parse input program...")
     input_program = sp.parse_prism_program(args.input)
+    logger.info("Build pomdp...")
     model = build_pomdp(input_program)
-    model = sp.pomdp.make_canonic(model)
 
     if len(model.initial_states) > 1:
         raise RuntimeError("We require a unique initial state")
-
+    logger.info("Write pomdp to file...")
     with open(args.output, 'w') as file:
         file.write("pomdp\n\n")
         file.write("observables\n")
@@ -48,6 +55,11 @@ def main():
                 file.write(" + ".join(updates))
                 file.write(";\n")
         file.write("endmodule\n")
+
+    logger.info("Check whether we can parse output...")
+    # As a simple check, we control whether storm can parse the output.
+    sp.parse_prism_program(args.output)
+    logger.info("Done.")
 
 
 if __name__ == "__main__":
